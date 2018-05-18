df62b47c336a61c275b5818c834fbea8b419f980
matthiasliszt <mliszt.program@gmail.com>
2018-05-18 14:05:39 +0200

    showing media from json

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+ $('#addMedia').click(function(){alert('add Media');});
+ $('#eraseMedia').click(function(){alert('erase Media');});
+
+ $('#showMedia').click(function(){
+  console.log('show Media');
+  var content=[];
+  function doRatingStars(n){
+   var stars=[];
+   for(var i=0;i<n;++i){stars.push('&starf;');}
+   if(n===0){stars.push('&#x2606;');}
+   return stars.join('');//returns a String
+  }
+  mediaData.map(function(o){var o1=mediaMask.replace("$$image$",o.image).replace("$$title$",o.title).replace("$$author$",o.author);
+                            var o2=o1.replace("$$type$",o.type).replace("$$genre$",o.genre).replace("$$publisher$",o.publisher);
+                            var stars=doRatingStars(o.rating);
+                            var output=o2.replace("$$description$",o.description).replace("$$rating$",stars);
+                              
+                            content.push(output);
+                            console.log(o.image);
+                           });
+  
+  $('#generalOutput').html(content);  
+ });
+ 
+});
---
+++ js/outputMask.js
@@ -0,0 +1,12 @@
+var mediaMask=`<div class="col-lg-4 col-sm-12">       
+               <h3>$$title$</h3>
+               <img src="$$image$" alt="not found!"/>
+               <p class="blue">$$genre$</p>
+               <div class="details"><h4><strong>Details</strong></h4>
+               <p>Author: $$author$</p> 
+               <p>Type: $$type$</p>
+               <p>Publisher: $$publisher$</p>
+               <textarea> $$description$</textarea> 
+               </div>  
+               <p>$$rating$</p>
+               </div>`;
